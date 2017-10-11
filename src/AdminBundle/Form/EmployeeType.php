a9cc4db679b955b92ace2a13faeb69925a904472
Bures <bures@modpreneur.com>
2017-10-11 16:43:18 +0200

    employee controller

---
+++ src/AdminBundle/Form/EmployeeType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AdminBundle\Form;
+
+use AppBundle\Entity\Employee;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EmployeeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('surname')
+            ->add('telephone');
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws \Symfony\Component\OptionsResolver\Exception\AccessException
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Employee::class,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_employee';
+    }
+
+
+}
