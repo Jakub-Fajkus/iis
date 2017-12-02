99485f5a8084f180f103a4862f99bb3511cc14a6
onsmak <onsmak@igloonet.cz>
2017-12-02 16:35:57 +0100

    align generate btn, add unique for pid and mid, fix error msg

--- src/AppBundle/Entity/Patient.php
+++ src/AppBundle/Entity/Patient.php
@@ -7,11 +7,15 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
 /**
  * Pacient
  *
  * @ORM\Table(name="patient")
  * @ORM\Entity(repositoryClass="PatientRepository")
+ * @UniqueEntity("personalIdentificationNumber")
+ * @UniqueEntity("medicalIdentificationNumber")
  */
 class Patient
 {
